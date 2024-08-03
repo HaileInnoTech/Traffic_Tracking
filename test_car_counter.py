5a3affe6a82982b01a1f481c6a89317ec3d53d67
HaileInnoTech <hai28022002@gmail.com>
2024-08-03 19:12:27 +0700

    update test

--- test_car_counter.py
+++ test_car_counter.py
@@ -25,3 +25,35 @@ def test_car_counter_orginal(setup):
     video.release()
     expected_car_count = 124
     assert car_counter == expected_car_count
+
+
+def test_car_counter_skipFrame(setup):
+    object_detector, tracker, pts, car_counter, seen_ids = setup
+    video = cv2.VideoCapture("traffic.mp4")
+    frame_count = 0
+    skip_frames = 2
+    while True:
+        ret, frame = video.read()
+        if not ret:
+            break
+
+        if frame_count % skip_frames == 0:
+            frame, mask, roi, car_counter, seen_ids = process_frame(frame, object_detector, tracker, pts, car_counter,
+                                                                    seen_ids)
+        frame_count += 1
+    video.release()
+    expected_car_count = 124
+    print(car_counter)
+    assert car_counter == expected_car_count
+
+def test_car_counter_orginalReverse(setup):
+    object_detector, tracker, pts, car_counter, seen_ids = setup
+    video = cv2.VideoCapture("trafficReverse.mp4")
+    while True:
+        ret, frame = video.read()
+        if not ret:
+            break
+        frame, mask, roi, car_counter, seen_ids = process_frame(frame, object_detector, tracker, pts, car_counter, seen_ids)
+    video.release()
+    expected_car_count = 124
+    assert car_counter == expected_car_count
